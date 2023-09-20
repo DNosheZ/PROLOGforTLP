3ddf1a9baad874e7fef98d030a76ee3a768a1143
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-09-20 08:56:24 -0500

    Create TravelNodes.pl

---
+++ TravelNodes.pl
@@ -0,0 +1,6 @@
+tramo(Medellin,Cartagena,Avion,200000).
+tramo(Cartagena,Maicao,Bus,60000).
+tramo(Maicao,Barrancabermeja,Bus,130000).
+tramo(Cali,Popayan,Bus,20000).
+tramo(Popayan,Puerto_Asis,Bus,90000).
+tramo(Puerto_Asis,Mitu,Lancha,150000).
